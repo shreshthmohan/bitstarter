dfac69330849c92137102150dcf531bdf8b5a538
Ubuntu <ubuntu@ip-172-31-11-251.us-west-2.compute.internal>
2014-03-31 08:32:36 +0000

    web.js was spitting index.html as a buffer, added buf.toString method to fix that

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var buf = new Buffer(50);
+buf.write(fs.readFileSync('index.html'), "utf-8");
 app.use(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    response.send(fs.readFileSync('index.html'));
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
