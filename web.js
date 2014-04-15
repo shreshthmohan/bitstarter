92ce91b74608ae12375d969e14afc2130e42ff42
Ubuntu <ubuntu@ip-172-31-11-251.us-west-2.compute.internal>
2014-04-15 06:52:08 +0000

    Modified fs.readFileSync call

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var buf = new Buffer(50);
-buf.write(fs.readFileSync('index.html', 'utf-8'), "utf-8");
+buf.write(fs.readFileSync('index.html'));
 app.use(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-  response.send(buf.toString('utf-8'));
+  response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
